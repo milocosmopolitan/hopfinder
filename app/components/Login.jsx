b3d06974d59c24145778944cabbdd497df6e6697
milocosmopolitan <milocosmopolitan@gmail.com>
2017-01-22 23:52:39 -0500

    before merge

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { login } from 'APP/app/reducers/auth';
+import { login } from '../reducers/auth';
 import { connect } from 'react-redux';
 import { Link, browserHistory } from 'react-router'
 
