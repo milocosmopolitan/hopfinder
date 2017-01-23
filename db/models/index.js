92a831add7de069734bce21ef44d2aa2dd53dad1
milocosmopolitan <milocosmopolitan@gmail.com>
2017-01-23 00:54:35 -0500

    heroku deply

--- db/models/index.js
+++ db/models/index.js
@@ -3,11 +3,10 @@
 // Require our models. Running each module registers the model into sequelize
 // so any other part of the application could call sequelize.model('User')
 // to get access to the User model.
-const db = require('APP/db')
 
 const User = require('./user')
-const Follow = require('./follow')
 const Breweries = require('./breweries')
+const Follow = require('./follow')
 // const Oauth = require('./oauth')
 
 Follow.belongsTo(User);
