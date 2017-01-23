592f1023da3c86a3db4184e5a535fcb0d769fd37
Daniel Kang <dkinsk@gmail.com>
2017-01-23 02:23:20 -0500

    addedinsta

--- app/routes/News.jsx
+++ app/routes/News.jsx
@@ -3,21 +3,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Grid, Row, Col } from 'react-bootstrap';
 import TweetEmbed from 'react-tweet-embed';
+import InstagramEmbed from 'react-instagram-embed';
 
 const tweets = ['822534725046771713', '823215006212902912', '798544956109586432', '821761864023547904', '821778798605639682', '823256735070744578', '821449984201658368', '822899750093684737', '822550575099289600', '823233450132066304', '822506541458673664']
 
+const insta = ['https://www.instagram.com/p/BPab5RPAuOO/', 'https://www.instagram.com/p/BPfuiGnBFgr/', 'https://www.instagram.com/p/BPlioWmB1Re/']
 
 const News = (props)=>{
 	return (
 			<Grid id="news-wrapper" fluid={true}>
-				<Row>
 					<Col xs={12} md={6} mdOffSet={3}>
-					{tweets.map(tweet => (
+					{
+						tweets.map(tweet => (
 						<TweetEmbed key={`${tweet}`} id={`${tweet}`} />
 						))
 					}
+					</Col>
+					<Col xs={12} md={6} mdOffSet={3}>
+					{
+						insta.map(inst => (
+							<InstagramEmbed key={`${insta}`} url={`${inst}`} maxWidth={400} />
+						))
+					}
 				</Col>
-			</Row>
 		</Grid>
 		)
 }
